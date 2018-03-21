3ed9ca29fdcaf59266a93b66fe857fc6dc980d72
Denis <corvus007@yandex.ru>
2018-03-21 11:27:17 +0300

    Добавляет в файл util.js метод declOfNum для склонения заданного слова

--- app/js/modules/util.js
+++ app/js/modules/util.js
@@ -1,6 +1,6 @@
-'use strict';
-
 window.util = (function () {
+  'use strict';
+
   return {
     setMaxHeight: function (selector) {
       var maxHeight;
@@ -18,6 +18,18 @@ window.util = (function () {
       Array.from(elements).forEach(function specifyMaxHeight(it) {
         it.style.height = maxHeight + 'px';
       });
+    },
+    declOfNum: function (titles) {
+      var number = Math.abs(number);
+      var cases = [2, 0, 1, 1, 1, 2];
+
+      return function (number) {
+        return titles[
+          number % 100 > 4 && number % 100 < 20 ?
+          2 :
+          cases[number % 10 < 5 ? number % 10 : 5]
+        ];
+      };
     }
   }
 })();
