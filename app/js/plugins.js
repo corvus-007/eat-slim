3276deb50dbbe965abfdb4ca0073c234db35064d
Denis <corvus007@yandex.ru>
2018-04-09 12:12:38 +0300

    Подключает GSAP и ScrollMagic

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -3,3 +3,6 @@
 //=require plugins/jquery.tabslet.min.js
 //=require plugins/jquery.maskedinput.min.js
 //=require plugins/nouislider.min.js
+//=require plugins/TweenMax.min.js
+//=require plugins/ScrollMagic.min.js
+//=require plugins/animation.gsap.min.js
