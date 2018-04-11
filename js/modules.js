cbcff04b7b02955a208fad3fdc860bdbf6d412ee
Denis <corvus007@yandex.ru>
2018-04-11 16:25:49 +0300

    Update 2018-04-11T13:25:43.833Z

--- js/modules.js
+++ js/modules.js
@@ -1,7 +1,10 @@
 window.util = (function () {
   'use strict';
 
+  var $ = window.jQuery;
+
   return {
+    KEYCODE_ESC: 27,
     setMaxHeight: function (selector) {
       var maxHeight;
       var elements = document.querySelectorAll(selector);
@@ -30,11 +33,172 @@ window.util = (function () {
           cases[number % 10 < 5 ? number % 10 : 5]
         ];
       };
+    },
+    getScrollbarWidth: function() {
+      var div = document.createElement('div');
+
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+
+      document.body.appendChild(div);
+      var scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+
+      return scrollWidth;
+    },
+  };
+})();
+
+window.calculator = (function () {
+  'use strict';
+
+  var $ = window.jQuery;
+  var formCalculator = document.querySelector('.form-calculator');
+  var FORM_CALCULATOR_CONTROLS_SELECTOR = '[data-calculator-role="control"]';
+  var FORM_CALCULATOR_OUTPUT_CALORIES_SELECTOR = '[data-calculator-role="output-calories"]';
+  var FORM_CALCULATOR_OUTPUT_TARIFF_SELECTOR = '[data-calculator-role="output-tariff"]';
+  var FORM_CALCULATOR_SUBMIT_SELECTOR = '.form-calculator__submit';
+  var RATIO_SLIM = 0.2;
+  var RATIO_MUSCLE = 0.1;
+  var RATIO_TONE = 0;
+  var tariffs = [{
+      id: 'lite',
+      name: 'Lite',
+      price: 500,
+      calories: 1200
+    },
+    {
+      id: 'lite-2',
+      name: 'Lite+',
+      price: 550,
+      calories: 1400
+    },
+    {
+      id: 'fitness',
+      name: 'Fitness',
+      price: 600,
+      calories: 1600
+    },
+    {
+      id: 'fitness-2',
+      name: 'Fitness+',
+      price: 650,
+      calories: 1800
     }
+  ];
+  var calories = 0;
+  var gender = '';
+  var personTarget = '';
+  var personWeight = 0;
+  var personHeight = 0;
+  var personAge = 0;
+  var coeffActivity = 0;
+  var tariffName = '';
+  var tariffId = '';
+  var tariffCalories = 0;
+
+  function calcCaloriesMan() {
+    return (
+      (9.99 * personWeight + 6.25 * personHeight - 4.92 * personAge + 5) *
+      coeffActivity
+    );
+  }
+
+  function calcCaloriesWoman() {
+    return (
+      (9.99 * personWeight + 6.25 * personHeight - 4.92 * personAge - 161) *
+      coeffActivity
+    );
+  }
+
+  function calcForm() {
+    var result = 0;
+    var resultTarget = 0;
+    gender = formCalculator.querySelector('input[name="gender"]:checked').value;
+    personTarget = formCalculator.querySelector('input[name="targetPerson"]:checked').value;
+    personWeight = formCalculator.weight.value ?
+      parseFloat(formCalculator.weight.value) :
+      0;
+    personHeight = formCalculator.height.value ?
+      parseFloat(formCalculator.height.value) :
+      0;
+    personAge = formCalculator.age.value ? parseFloat(formCalculator.age.value) : 0;
+    coeffActivity = parseFloat(formCalculator.querySelector('input[name="activity"]:checked').value);
+
+    if (!personWeight || !personHeight || !personAge) {
+      return 0;
+    }
+
+    switch (gender) {
+      case 'male':
+        result = calcCaloriesMan();
+        break;
+      case 'female':
+        result = calcCaloriesWoman();
+        break;
+      default:
+        break;
+    }
+
+    switch (personTarget) {
+      case 'slim':
+        result -= result * RATIO_SLIM;
+        break;
+      case 'muscle':
+        result += result * RATIO_MUSCLE;
+        break;
+      case 'tone':
+        result += result * RATIO_TONE;
+        break;
+      default:
+        break;
+    }
+
+    return result;
+  }
+
+  if (formCalculator) {
+    calories = Math.round(calcForm());
+    $(FORM_CALCULATOR_OUTPUT_CALORIES_SELECTOR).text(calories);
+
+    $(formCalculator).on('change input', FORM_CALCULATOR_CONTROLS_SELECTOR, function (event) {
+      calories = Math.round(calcForm());
+      $(FORM_CALCULATOR_OUTPUT_CALORIES_SELECTOR).text(calories);
+      if (calories > 0) {
+        for (var i = 0; i < tariffs.length; i++) {
+          tariffCalories = tariffs[i].calories;
+
+          if (calories < tariffCalories) {
+            tariffName = tariffs[i].name;
+            tariffId = tariffs[i].id;
+            break;
+          }
+        }
+
+        if (!tariffName) {
+          tariffName = tariffs[tariffs.length-1].name;
+          tariffId = tariffs[tariffs.length-1].id;
+        }
+      }
+      $(FORM_CALCULATOR_OUTPUT_TARIFF_SELECTOR).text(tariffName);
+      $(FORM_CALCULATOR_SUBMIT_SELECTOR).find('span').text('Заказать')
+      console.log(tariffName);
+    });
+
+    $(formCalculator).on('submit', function(event) {
+      event.preventDefault();
+      var action = this.action;
+      document.location.href = action + '?order_tariff=' + tariffId;
+    });
   }
 })();
 
 window.newOrder = (function () {
+  'use strict';
+
+  var $ = window.jQuery;
   var formNewOrder = document.querySelector('.form-new-order');
 
   if (!formNewOrder) {
@@ -45,10 +209,16 @@ window.newOrder = (function () {
   var TOTAL_SUM_SELECTOR = '[data-order-role="total-sum"]';
   var REBATE_SELECTOR = '[data-order-role="rebate"]';
   var TARIFF_PRICE_SELECTOR = '[data-tarif-price]';
+  var DAYS_INPUT_SELECTOR = '[data-order-role="days-input"]';
+  var TOTAL_SUM_INPUT_SELECTOR = '[data-order-role="total-input"]';
+  var REBATE_INPUT_SELECTOR = '[data-order-role="rebate-input"]';
   var CONTROLS_SELECTOR = '[data-order-role="control"]';
-  var rebate = 10 / 100;
+  var rebatePercent = 10;
+  var rebate = rebatePercent / 100;
   var price = 0;
   var days = 0;
+  var currentTotal = 0;
+  var isRebate = false;
 
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -62,9 +232,11 @@ window.newOrder = (function () {
 
   function calcOrder() {
     var result = price * days;
+    isRebate = false;
 
     if (days === 7) {
       result -= rebate * result;
+      isRebate = true;
     }
 
     return result;
@@ -108,17 +280,102 @@ window.newOrder = (function () {
   formNewOrderSlider.noUiSlider.on('update', function (values, handle) {
     price = $(TARIFF_PRICE_SELECTOR).filter(':checked').data('tarif-price');
     days = parseInt(values[handle], 10);
+    currentTotal = calcOrder();
 
     $(REBATE_SELECTOR).html(updateRebateText(days));
-    $(TOTAL_SUM_SELECTOR).html(calcOrder() + ' руб');
+    $(DAYS_INPUT_SELECTOR).val(days);
+    $(TOTAL_SUM_SELECTOR).html(currentTotal + ' руб');
+    $(TOTAL_SUM_INPUT_SELECTOR).val(currentTotal + ' руб');
+    if (isRebate) {
+      $(REBATE_INPUT_SELECTOR).val(rebatePercent + '%');
+    } else {
+      $(REBATE_INPUT_SELECTOR).val('0%');
+    }
   });
 
   $(formNewOrder).on('change input', CONTROLS_SELECTOR, function (event) {
     price = $(TARIFF_PRICE_SELECTOR).filter(':checked').data('tarif-price');
     days = parseInt(formNewOrderSlider.noUiSlider.get());
+    currentTotal = calcOrder();
 
     $(REBATE_SELECTOR).html(updateRebateText(days));
-    $(TOTAL_SUM_SELECTOR).text(calcOrder() + ' руб');
+    $(DAYS_INPUT_SELECTOR).val(days);
+    $(TOTAL_SUM_SELECTOR).html(currentTotal + ' руб');
+    $(TOTAL_SUM_INPUT_SELECTOR).val(currentTotal + ' руб');
+    if (isRebate) {
+      $(REBATE_INPUT_SELECTOR).val(rebatePercent + '%');
+    } else {
+      $(REBATE_INPUT_SELECTOR).val('0%');
+    }
   });
+})();
+
+window.outCover = (function() {
+  'use strict';
+
+  var $ = window.jQuery;
+  var outCover = document.querySelector('.out-cover');
+  var outCoverCloseWrapper = outCover.querySelector(
+    '.out-cover__close-wrapper'
+  );
+  var outCoverBody = outCover.querySelector('.out-cover__body');
+  var outCoverMainNavWrapper = outCover.querySelector(
+    '.out-cover__main-nav-wrapper'
+  );
+  var outCoverFooter = outCover.querySelector('.out-cover__footer');
+  var outCoverToggler = document.querySelector('.out-cover-toggler');
+  var outCoverClose = outCover.querySelector('.out-cover__close');
+  var scrollWidth = window.util.getScrollbarWidth();
+  var scrollPageValue = 0;
+
+  var onOutCoverEscPress = function(event) {
+    if (event.keyCode === window.util.KEYCODE_ESC) {
+      hideOutCover();
+    }
+  };
+
+  var getSumHeight = function() {
+    var result = 0;
+
+    for (var i = 0; i < arguments.length; i++) {
+      result += arguments[i].offsetHeight;
+    }
+
+    return result;
+  };
+
+  var showOutCover = function() {
+    scrollPageValue = window.pageYOffset;
+    outCoverBody.style.height =
+      'calc(100% - (' +
+      getSumHeight(outCoverCloseWrapper, outCoverFooter) +
+      'px))';
+    document.body.classList.add('no-scroll');
+    outCover.classList.add('out-cover--opened');
+    document.addEventListener('keydown', onOutCoverEscPress);
+  };
+
+  var hideOutCover = function() {
+    outCover.classList.remove('out-cover--opened');
+    document.body.classList.remove('no-scroll');
+    window.scrollTo(0, scrollPageValue);
+    document.removeEventListener('keydown', onOutCoverEscPress);
+  };
+
+  outCoverToggler.addEventListener('click', function(event) {
+    event.preventDefault();
+    showOutCover();
+  });
+
+  outCoverClose.addEventListener('click', function(event) {
+    event.preventDefault();
+    hideOutCover();
+  });
+
+  return {
+    mainNavWrapper: outCoverMainNavWrapper,
+    show: showOutCover,
+    hide: hideOutCover
+  };
 })();
 
--- js/script.js
+++ js/script.js
@@ -1,15 +1,118 @@
 document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  var $ = window.jQuery;
+  var POPUP_MODIFY_SHOW_CLASS = 'popup--open';
+  var BODY_POPUP_SHOWED_CLASS = 'is-popup-open';
+  var controller = new ScrollMagic.Controller();
+  var scene;
+
+  $('.welcome__to-choose-tariff').on('click', function (event) {
+    event.preventDefault();
+
+    var targetSection = $(this).attr('href');
+    var targetOffset = $(targetSection).offset().top;
+
+    TweenMax.to(window, 1, {
+      scrollTo: {
+        y: targetOffset
+      },
+      ease: Power3.easeOut
+    });
+  });
+
   svg4everybody();
+
+  var tlHeader = new TimelineLite();
+  tlHeader
+    .staggerFrom('.page-header__inner > *', 0.5, {
+      y: -50,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    }, 0.3, 0.3)
+    .staggerFrom('.welcome__title, .welcome__subtitle, .welcome__text, .welcome__actions', 0.5, {
+      autoAlpha: 0,
+      x: 20,
+      ease: Back.easeOut.config(1.4)
+    }, 0.2, '-=0.4')
+    .from('.welcome__dish', 0.5, {
+      autoAlpha: 0,
+      scale: 0.9,
+      x: 100,
+      ease: Back.easeOut.config(1.4)
+    });
+
+
+  var tlHowIt = new TimelineLite();
+  tlHowIt.staggerFrom('.how-it-work__item', 0.8, {
+    y: -100,
+    autoAlpha: 0,
+    ease: Back.easeOut.config(1.4)
+  }, 0.3);
+  scene = new ScrollMagic.Scene({
+      triggerElement: '.how-it-work',
+      reverse: false
+    })
+    .setTween(tlHowIt)
+    .addTo(controller);
+
+
+  var tlWeeklyMenu = new TimelineLite();
+  tlWeeklyMenu
+    .from('.tariff-menu-tabs__menu', 0.5, {
+      y: 50,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    })
+    .from('.weekly-menu-tabs__menu', 0.5, {
+      y: 50,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    }, '-=0.3')
+    .staggerFrom('.tariff-menu-tabs__pane:first-of-type .weekly-menu-tabs__pane:first-of-type .menu-for-day__item', 0.5, {
+      y: 50,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    }, 0.2)
+    .from('.menu-for-day__footer', 0.5, {
+      y: 50,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    });
+  scene = new ScrollMagic.Scene({
+      triggerElement: '.weekly-menu',
+      reverse: false
+    })
+    .setTween(tlWeeklyMenu)
+    .addTo(controller);
+
+
+  var tlTariffPlans = new TimelineLite();
+  tlTariffPlans
+    .staggerFrom('.tariff-plans__item', 0.7, {
+      y: 100,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.5)
+    }, 0.3)
+    .from('.tariff-plans__footer', 0.6, {
+      y: 40,
+      autoAlpha: 0,
+      ease: Back.easeOut.config(1.4)
+    });
+  scene = new ScrollMagic.Scene({
+      triggerElement: '.tariff-plans',
+      reverse: false
+    })
+    .setTween(tlTariffPlans)
+    .addTo(controller);
 
   $('input[type="tel"]').mask('+7 (999) 999-99-99', {});
 
-  var POPUP_MODIFY_SHOW_CLASS = 'popup--open';
-  var BODY_POPUP_SHOWED_CLASS = 'is-popup-open';
-
+  $('.tariff-menu-tabs').tabslet();
   $('.weekly-menu-tabs').tabslet();
 
   if (window.matchMedia("(max-width: 991px)").matches) {
-    $('.weekly-menu-tabs').on('_after', function () {
+    $('.tariff-menu-tabs, .weekly-menu-tabs').on('_after', function () {
       $('.menu-for-day-slider').flickity('resize');
     });
 
@@ -19,25 +122,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  function getScrollBarWidth() {
-    // создадим элемент с прокруткой
-    var div = document.createElement('div');
-
-    div.style.overflowY = 'scroll';
-    div.style.width = '50px';
-    div.style.height = '50px';
-
-    // при display:none размеры нельзя узнать
-    // нужно, чтобы элемент был видим,
-    // visibility:hidden - можно, т.к. сохраняет геометрию
-    div.style.visibility = 'hidden';
-
-    document.body.appendChild(div);
-    var scrollWidth = div.offsetWidth - div.clientWidth;
-    document.body.removeChild(div);
-
-    return scrollWidth;
-  }
+  $('.reviews-slider').flickity({
+    wrapAround: true,
+    dragThreshold: 10
+  });
 
   function showPopup(popupSelector) {
     var popup = document.querySelector(popupSelector);
@@ -45,7 +133,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // document.body.classList.add(BODY_POPUP_SHOWED_CLASS);
     document.body.style.overflow = 'hidden';
-    document.body.style.paddingRight = getScrollBarWidth() + 'px';
+    document.body.style.paddingRight = window.util.getScrollbarWidth() + 'px';
     popup.classList.add(POPUP_MODIFY_SHOW_CLASS);
 
     popupClose.addEventListener('click', function (event) {
@@ -64,77 +152,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   var formCalculator = document.querySelector('.form-calculator');
-  var FORM_CALCULATOR_CONROLS_SELECTOR = '[data-calculator-role="control"]';
+  var FORM_CALCULATOR_CONTROLS_SELECTOR = '[data-calculator-role="control"]';
   var FORM_CALCULATOR_OUTPUT_SELECTOR = '[data-calculator-role="output"]';
-  var RATIO_SLIM = 0.2;
-  var RATIO_MUSCLE = 0.1;
-  var RATIO_TONE = 0;
-  var gender = '';
-  var personTarget = '';
-  var personWeight = 0;
-  var personHeight = 0;
-  var personAge = 0;
-  var coeffActivity = 0;
-
-  function calcFormForMan() {
-    return (
-      (9.99 * personWeight + 6.25 * personHeight - 4.92 * personAge + 5) *
-      coeffActivity
-    );
-  }
-
-  function calcFormForWoman() {
-    return (
-      (9.99 * personWeight + 6.25 * personHeight - 4.92 * personAge - 161) *
-      coeffActivity
-    );
-  }
-
-  function calcForm() {
-    var result = 0;
-    var resultTarget = 0;
-    gender = formCalculator.querySelector('input[name="gender"]:checked').value;
-    personTarget = formCalculator.querySelector('input[name="targetPerson"]:checked').value;
-    personWeight = formCalculator.weight.value ?
-      parseFloat(formCalculator.weight.value) :
-      0;
-    personHeight = formCalculator.height.value ?
-      parseFloat(formCalculator.height.value) :
-      0;
-    personAge = formCalculator.age.value ? parseFloat(formCalculator.age.value) : 0;
-    coeffActivity = parseFloat(formCalculator.querySelector('input[name="activity"]:checked').value);
-
-    if (!personWeight || !personHeight || !personAge) {
-      return 0;
-    }
-
-    switch (gender) {
-      case 'male':
-        result = calcFormForMan();
-        break;
-      case 'female':
-        result = calcFormForWoman();
-        break;
-      default:
-        break;
-    }
-
-    switch (personTarget) {
-      case 'slim':
-        result -= result * RATIO_SLIM;
-        break;
-      case 'muscle':
-        result += result * RATIO_MUSCLE;
-        break;
-      case 'tone':
-        result += result * RATIO_TONE;
-        break;
-      default:
-        break;
-    }
-
-    return result;
-  }
 
   $('[data-popup-target]').on('click', function (event) {
     event.preventDefault();
@@ -143,13 +162,4 @@ document.addEventListener('DOMContentLoaded', function () {
       showPopup(popupTargetSelector);
     }
   });
-
-  if (formCalculator) {
-    $(FORM_CALCULATOR_OUTPUT_SELECTOR).text(Math.round(calcForm()));
-    $(formCalculator).on('change input', FORM_CALCULATOR_CONROLS_SELECTOR, function (
-      event
-    ) {
-      $(FORM_CALCULATOR_OUTPUT_SELECTOR).text(Math.round(calcForm()));
-    });
-  }
 });
