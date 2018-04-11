0d3ddc4985ef181190db19e8e7534aa7e6580b56
Denis <corvus007@yandex.ru>
2018-04-11 16:20:21 +0300

    Обновляет util.js

--- app/js/modules/util.js
+++ app/js/modules/util.js
@@ -4,6 +4,7 @@ window.util = (function () {
   var $ = window.jQuery;
 
   return {
+    KEYCODE_ESC: 27,
     setMaxHeight: function (selector) {
       var maxHeight;
       var elements = document.querySelectorAll(selector);
@@ -32,6 +33,20 @@ window.util = (function () {
           cases[number % 10 < 5 ? number % 10 : 5]
         ];
       };
-    }
+    },
+    getScrollbarWidth: function() {
+      var div = document.createElement('div');
+
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+
+      document.body.appendChild(div);
+      var scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+
+      return scrollWidth;
+    },
   };
 })();
